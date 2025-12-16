7e10dcdf59938090bffd6847347e8e093606bfec
sh1v-max <singhshiv0427@gmail.com>
2025-12-16 23:16:35 +0530

    explanation of throttle

--- 02-Throttle/index.js
+++ 02-Throttle/index.js
@@ -1,5 +1,7 @@
 // throttling is a technique used to control how many times we allow a function to be executed over time. when a javaScript function is said to be throttled with a wait time of X milliseconds. the callback is invoked immediately and cannot be invoked again for the rest of the wait duration
 
+// throttle: run -> lock, wait X ms -> unlock, run -> lock, wait X ms -> unlock, run ...
+
 // example:
 
 let i = 0;
@@ -23,21 +25,25 @@ throttledIncrement(); // i = 2
 // =======================================================
 // implementation of throttle function
 
-export default function throttle(func, wait) {
-  // throw 'Not implemented!';
-  // forming a closure to remember whether the fun is blocked
-  let isThrottled = false;
+function throttle(func, wait) {
+    let isThrottled = false
+    // if it's false, function is allowed to run
+    // if true, function is blocked
+    // it forms closure
 
-  // first call, run immediately
-  return function (...args) {
-    if (isThrottled) return;
+    // because throttle function returns a function
+    return function (...args) {
+        if (isThrottled) return
 
-    func.apply(this, args);
-    isThrottled = true;
+        // executed the original function, passes the argument
+        func.apply(this, args)
+        // set is to false after executing the function
+        isThrottled = true
 
-    // after wait ms, run again
-    setTimeout(() => {
-      isThrottled = false;
-    }, wait);
-  }
+        // now runs timeout
+        // fun becomes callable again after wait time
+        setTimeout(() => {
+            isThrottled = false
+        }, wait)
+    }
 }
